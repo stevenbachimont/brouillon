6a8fa0b1961e6d1936553cb6376ed2cfc17d193d
stevenbachimont <stevenbachimont@gmail.com>
2024-06-06 10:22:36 +0200

    reussite photos2

--- src/components/Photo2.jsx
+++ src/components/Photo2.jsx
@@ -29,6 +29,8 @@ export const Photo2 = () => {
         geo.getCurrentPosition((position) => {
             setLatitude(position.coords.latitude);
             setLongitude(position.coords.longitude);
+        }, (error) => {
+            console.log(error);
         });
     }, []);
 
@@ -48,14 +50,18 @@ export const Photo2 = () => {
                     videoConstraints={videoConstraints}
                 />
                 <button onClick={capture}>Capture photo</button>
+
                 {capturedImages.map((image, index) => (
-                    <div key={index}>
+                    <div className="recu" key={index}>
                         <img className="prise" src={image.src} alt="Captured" />
+                        <div className="mapouter">
                         <p>Latitude: {image.latitude}</p>
                         <p>Longitude: {image.longitude}</p>
-                        <p>Date et heure: {image.timestamp.toLocaleString()}</p>
+                        <p>{image.timestamp.toLocaleString()}</p>
+                        </div>
                     </div>
                 ))}
+
             </card>
         </>
     );
